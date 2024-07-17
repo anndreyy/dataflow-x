b4a681122293bbbe5e9b6b7abf8a74d045a52664
anndreyy <anndreyy@live.com>
2024-07-17 19:58:28 -0300

    feat: inclusao do sistema de agendamento

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,4 +1,4 @@
-const { connectToSql, fetchData } = require('../models/sqlModel');
+const { connectToSql, fetchData, closeConnection } = require('../models/sqlModel');
 const { inferSchemaFromDb } = require('../services/schemaService');
 const { ensureDatasetExists, loadToBigQuery } = require('../services/bigQueryService');
 const { cloudSchedulerJobCreate, cloudSchedulerJobDelete } = require('../services/cloudScheduler');
@@ -7,16 +7,20 @@ const package = require('../../package.json');
 async function processData(req, res) {
     var form = req.body;
 
+    // set name
+    form.name = `${form.origin}_${form.tableId}`;
+    if (form.schemaDb !== 'dbo') form.name = `${form.origin}_${form.schemaDb}_${form.tableId}`;
+
     const io = req.app.get('socketio');
     io.log = function (...args) { io.emit('status', args.join(' ')); console.log(...args) }
 
     try {
         await connectToSql(form.origin, io);
 
-        const data = await fetchData(form.schemaDb, form.tableId, io);
+        const data = await fetchData(form.origin, form.schemaDb, form.tableId, io);
 
         if (!form.schema) {
-            var dataSchema = await inferSchemaFromDb(form.schemaDb, form.tableId);
+            var dataSchema = await inferSchemaFromDb(form.origin, form.schemaDb, form.tableId);
             form.schema = dataSchema.inferred;
             io.log(`Esquema inferido do SQL Server, ${form.schema.length} colunas`);
         }
@@ -36,21 +40,21 @@ async function processData(req, res) {
         await ensureDatasetExists(form.schemaBq);
         io.log('Dataset BigQuery garantido');
 
-        await loadToBigQuery(form.schemaBq, `${form.origin}_${form.tableId}`, data, form.schema, form.isDelete, form.isCreate, 20, io);
-        io.log('Dados carregados no BigQuery');
+        // Parametro para o processamento do bigQuery ser async
+        if(!form?.isAsync) {
 
+            await loadToBigQuery(form.schemaBq, form.name, data, form.schema, form.isDelete, form.isCreate, 20, io);
+            io.log('Dados carregados no BigQuery');
+        } else {
+            loadToBigQuery(form.schemaBq, form.name, data, form.schema, form.isDelete, form.isCreate, 20, io);
+        }
 
         if (form.isSchedule) {
             io.log('Criando job no Cloud Scheduler');
             const jobData = {
-                jobName: `${package.name}_${form.origin}_${form.tableId}`,
-                timeZone: 'America/Sao_Paulo',
+                jobName: `${package.name}_${form.name}`,
                 schedule: form.scheduleCron,
                 uri: `${process.env.BASE_URL}/api/process-data`,
-                httpMethod: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
                 body: {
                     tableId: form.tableId,
                     schemaDb: form.schemaDb,
@@ -58,25 +62,38 @@ async function processData(req, res) {
                     isDelete: form.isDelete,
                     isCreate: form.isCreate,
                     origin: form.origin,
-                    schema: form.schema
+
+                    // Schema só é enviado se tiver sido modificado
+                    schema: form.isManualSchema ? form.schema : false,
+
+                    // Sinaliza para integração ser async, permitido que a requisição 
+                    // seja finalizada antes do processamento ser concluído
+                    // evitando timeout
+                    isAsync: true,
                 }
             };
 
-            // replace _ por -
             jobData.jobName = jobData.jobName.replace(/_/g, '-');
 
-            const resCloudSchedulerJobDelete = await cloudSchedulerJobDelete(jobData.jobName, io);
+            await cloudSchedulerJobDelete(jobData.jobName, io);
             io.log('Job deletado do Cloud Scheduler');
-            const resCloudSchedulerJobCreate = await cloudSchedulerJobCreate(jobData, io);
+            await cloudSchedulerJobCreate(jobData, io);
             io.log('Job criado no Cloud Scheduler');
         }
 
-        io.log(`link: https://console.cloud.google.com/bigquery?ws=!1m5!1m4!4m3!1sssot-391717!2sraw!3s${form.origin}_${form.tableId}&project=ssot-391717`);
+        form.link = `https://console.cloud.google.com/bigquery?ws=!1m5!1m4!4m3!1sssot-391717!2sraw!3s${form.name}&project=ssot-391717`;
 
-        res.json({ message: 'Dados processados e carregados no BigQuery com sucesso.' });
+        io.log(`link: ${form.link}`);
+
+        return res.json({
+            message: 'Dados processados e carregados no BigQuery com sucesso.',
+            form,
+            status: true
+        });
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: err.message });
+        console.error(err); 
+        closeConnection();
+        return res.status(500).json({ error: err.message, status: false });
     }
 }
 
